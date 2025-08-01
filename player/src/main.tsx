47f8ea5565a808fa6b2b4c6a5adfe8e3293391ba
Santiago Jiménez H <sj@sjim.dev>
2025-08-01 15:50:18 +0200

    Add i18n support with react-i18next

--- player/src/main.tsx
+++ player/src/main.tsx
@@ -5,6 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import theme from './config/theme.ts'
 import './index.css'
 import App from './App.tsx'
+import './config/i18n'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
