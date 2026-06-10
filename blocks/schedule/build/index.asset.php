f04c122283c28d1482aa426036c89cf1ef564a79
Santiago Jiménez H. <sj@sjim.dev>
2026-06-10 17:46:37 +0200

    Update version in asset files and enhance block settings for Radio Schedule

--- blocks/schedule/build/index.asset.php
+++ blocks/schedule/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '60d0a31e9710a009bd2f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '701f3b541b409ecfccd0');
