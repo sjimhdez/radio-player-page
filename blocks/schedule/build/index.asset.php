38392c5c26de5155ad0f16bfb58acd88bb446dd7
Santiago Jiménez H. <sj@sjim.dev>
2026-08-03 00:30:42 +0200

    Compile blocks

--- blocks/schedule/build/index.asset.php
+++ blocks/schedule/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '1327fd750277baaac7ad');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '8096349e9882ae5915f1');
